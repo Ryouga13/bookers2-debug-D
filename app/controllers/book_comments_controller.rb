b006ef78c5fe5e861bb5b8fd639ea00353ce5a49
EC2 Default User <ec2-user@ip-172-31-37-105.eu-north-1.compute.internal>
2024-12-02 03:55:53 +0000

    bookers2-debug6 20241202

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,21 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = BookComment.new(comment_params)
-    book_comment.user_id = current_user.id
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    unless @book_comment.save
+      render 'error'  # app/views/book_comments/error.js.erbを参照する
+    end
+    # app/views/book_comments/create.js.erbを参照する
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     book_comment = BookComment.find(params[:id])
+    book_comment.book_id = @book.id
     book_comment.destroy
-    redirect_to book_path(params[:book_id])
+    # app/views/book_comments/destroy.js.erbを参照する
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,19 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     favorite = Favorite.new
     favorite.user_id = current_user.id
-    favorite.book_id = book.id
+    favorite.book_id = @book.id
     favorite.save
-    redirect_back(fallback_location: root_path)
-
+    # app/views/favorites/create.js.erbを参照する
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = Favorite.find_by(user_id: current_user.id, book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # app/views/favorites/destroy.js.erbを参照する
   end
 
 
